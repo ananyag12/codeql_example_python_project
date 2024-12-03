8758aa02f3273f95ea357f51c3c20b0b3efbe391
Ananya Gollakota <76816318+ananyag12@users.noreply.github.com>
2024-12-03 15:55:57 -0500

    Create leapyear.py

---
+++ test13/leapyear.py
@@ -0,0 +1,13 @@
+def main():
+    try:
+        year = int(input("Enter a year: "))
+        if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+            print(f"{year} is a leap year.")
+        else:
+            print(f"{year} is not a leap year.")
+            
+    except ValueError:
+        print("Invalid input! Please enter a string.")
+
+if __name__ == "__main__":
+    main()
