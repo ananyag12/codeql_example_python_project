8a56d6fcfe53af933093445f4273ef0578f496f4
Ananya Gollakota <76816318+ananyag12@users.noreply.github.com>
2024-12-05 22:55:16 -0500

    Create number.py

---
+++ test14/number.py
@@ -0,0 +1,11 @@
+def main():
+    num = float(input("Enter a number: "))
+    if num > 0:
+        print("Positive")
+    elif num < 0:
+        print("Negative")
+    else:
+        print("Zero")
+
+if __name__ == "__main__":
+    main()
